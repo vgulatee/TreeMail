d00618e284d1965c4711972a3053c87ecb63e6ce
vgulatee <vishesh_g@hotmail.com>
2017-10-31 17:48:52 -0400

    Secure API keys

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express'); //Import express
+const passport = require('passport'); //Import passport
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //Import strategy
+
 const app= express(); //Generate new express app
 
-app.get('/', (req, res)=>{ //Running express server to run route handler 
-    res.send({bye: 'buddy'}); //Rounte handler request responds with creating a JSON object 'hi:there"
-});
+
+
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000; //Web deploy app uses whatever port is dynamically given, if not defaults to por: 5000
 app.listen(PORT);
