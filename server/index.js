5607dbf2987470fbf1c14c2643818be273abd319
vgulatee <vishesh_g@hotmail.com>
2017-10-31 16:21:46 -0400

    Future deploy test

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express'); //Import express
 const app= express(); //Generate new express app
 
 app.get('/', (req, res)=>{ //Running express server to run route handler 
-    res.send({hi: 'there'}); //Rounte handler request responds with creating a JSON object 'hi:there"
+    res.send({bye: 'buddy'}); //Rounte handler request responds with creating a JSON object 'hi:there"
 });
 
 const PORT = process.env.PORT || 5000; //Web deploy app uses whatever port is dynamically given, if not defaults to por: 5000
