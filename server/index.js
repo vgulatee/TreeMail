243f86bcd397a0ab9290e0e87604afafd55925b1
vgulatee <vishesh_g@hotmail.com>
2017-11-01 01:16:31 -0400

    Phase one of googleAuth done

--- server/index.js
+++ server/index.js
@@ -1,27 +1,8 @@
 const express = require('express'); //Import express
-const passport = require('passport'); //Import passport
-const GoogleStrategy = require('passport-google-oauth20').Strategy; //Import strategy
-const keys = require('./config/keys');
+require('./services/passport');
 
 const app = express(); //Generate new express app
-
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
- 
-}));
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000; //Web deploy app uses whatever port is dynamically given, if not defaults to por: 5000
 app.listen(PORT);
---
+++ server/services/passport.js
@@ -0,0 +1,14 @@
+const passport = require('passport'); //Import passport
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //Import strategy
+const keys = require('../config/keys');
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile', profile);
+  
+}));
