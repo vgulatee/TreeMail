a61f19c01bbfceda70f802407d3f3553ef6a5577
vgulatee <vishesh_g@hotmail.com>
2017-11-01 02:24:29 -0400

    Mongo-mongoose setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express'); //Import express
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
 const app = express(); //Generate new express app
+mongoose.connect(keys.mongoURI);
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000; //Web deploy app uses whatever port is dynamically given, if not defaults to por: 5000
+const PORT = process.env.PORT; //Web deploy app uses whatever port is dynamically given, if not defaults to por: 5000
 app.listen(PORT);
